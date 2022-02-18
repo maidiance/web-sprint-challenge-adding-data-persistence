915ffb5c3f94a008a405741e70df9ff6a96b06ff
Winifred Lu <winnie.lu@comcast.net>
2022-02-18 08:25:24 -0500

    first migration file made

---
+++ data/migrations/20220218131713_projects.js
@@ -0,0 +1,48 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('projects', tbl => {
+        tbl.increments('project_id');
+        tbl.string('project_name').notNullable();
+        tbl.string('project_description');
+        tbl.boolean('project_completed').defaultTo('false');
+    })
+    .createTable('resources', tbl => {
+        tbl.increments('resource_id');
+        tbl.string('resource_name').notNullable().unique();
+        tbl.string('resource_description');
+    })
+    .createTable('tasks', tbl=> {
+        tbl.increments('task_id');
+        tbl.string('task_description').notNullable();
+        tbl.string('task_notes');
+        tbl.boolean('task_completed').defaultTo('false');
+        tbl.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects')
+            .onDelete('CASCADE');
+    })
+    .createTable('resource_assignment', tbl=> {
+        tbl.integer('resource_id')
+            .unsigned()
+            .notNullable()
+            .references('resource_id')
+            .inTable('resources');
+        tbl.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects');
+        tbl.primary(['resource_id', 'project_id']);
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExists('resource_assignment')
+        .dropTableIfExists('tasks')
+        .dropTableIfExists('resources')
+        .dropTableIfExists('projects');
+};
