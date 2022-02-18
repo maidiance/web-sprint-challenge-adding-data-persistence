07e73091f4114b9faa330dd5be3697f77f26e8f7
Winifred Lu <winnie.lu@comcast.net>
2022-02-18 09:27:25 -0500

    debugging

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 // build your server here and require it from index.js
 const express = require('express');
 const helmet = require('helmet');
-const projectRouter = require('./resource/router');
+const projectRouter = require('./project/router');
 const resourceRouter = require('./resource/router');
 const taskRouter = require('./task/router');
 
@@ -9,8 +9,8 @@ const server = express();
 
 server.use(helmet());
 server.use(express.json());
-server.use('/api/resources', resourceRouter);
 server.use('/api/projects', projectRouter);
+server.use('/api/resources', resourceRouter);
 server.use('/api/tasks', taskRouter);
 
 server.use((err, req, res, next) => { // eslint-disable-line
