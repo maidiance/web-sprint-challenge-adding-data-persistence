55a81d814b887236f121f7a20fd79460a9ee6c8a
Winifred Lu <winnie.lu@comcast.net>
2022-02-18 08:43:26 -0500

    basic router for project complete

--- api/project/router.js
+++ api/project/router.js
@@ -5,7 +5,24 @@ const Projects = require('./model');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    
+    Projects.get()
+        .then(projects => {
+            res.json(projects);
+        })
+        .catch(() => {
+            res.status(500).json({message: 'could not get projects'});
+        })
+})
+
+router.post('/', (req, res) => {
+    const project = req.body;
+    Projects.add(project)
+        .then(project => {
+            res.status(201).json(project);
+        })
+        .catch(() => {
+            res.status(500).json({message: 'could not post project'});
+        })
 })
 
 module.exports = router;
