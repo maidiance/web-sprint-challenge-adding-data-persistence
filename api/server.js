61412848f0042c8a68ea14b53208a296ea84e0c4
Winifred Lu <winnie.lu@comcast.net>
2022-02-18 08:09:07 -0500

    basic index and server set up

--- api/server.js
+++ api/server.js
@@ -1,1 +1,21 @@
 // build your server here and require it from index.js
+const express = require('express');
+const helmet = require('helmet');
+const projectRouter = require('./resource/router');
+const resourceRouter = require('./resource/router');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use('/api/resource', resourceRouter);
+server.use('/api/project', projectRouter);
+
+server.use((err, req, res, next) => { // eslint-disable-line
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
