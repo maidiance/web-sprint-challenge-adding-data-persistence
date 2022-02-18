92efd04238d62c20f85492f54094577b0ea5789d
Winifred Lu <winnie.lu@comcast.net>
2022-02-18 08:32:26 -0500

    models in progress

--- api/project/model.js
+++ api/project/model.js
@@ -1,3 +1,10 @@
 // build your `Project` model here
 const db = require('../data/db-config');
 
+function get() {
+    return db('projects as p');
+}
+
+module.exports = {
+    get
+};
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,11 @@
 // build your `/api/projects` router here
+const express = express;
+const Projects = require('./model');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    
+})
+
+module.exports = router;
