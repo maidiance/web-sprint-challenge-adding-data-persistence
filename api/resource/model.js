74622b682d30d014a4bef54ce7704b1326909c8e
Winifred Lu <winnie.lu@comcast.net>
2022-02-18 08:39:08 -0500

    basic model for resource complete

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,28 @@
 // build your `Resource` model here
+const db = require('../data/db-config');
+
+function get() {
+    return db('resources');
+}
+
+async function getById(id) {
+    const result = await db('resources')
+        .where('resource_id', id);
+    if(result.length < 1) {
+        return null;
+    }
+    return result;
+}
+
+function add(resource) {
+    return db('resources')
+        .insert(resource)
+        .then(([id]) => {
+            return getById(id);
+        })
+}
+
+module.exports = {
+    get,
+    add
+};
