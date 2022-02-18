19d9f671d20adb839487c64d33d29d2f35202a27
Winifred Lu <winnie.lu@comcast.net>
2022-02-18 08:40:02 -0500

    basic model for task complete

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,29 @@
 // build your `Task` model here
+// build your `Resource` model here
+const db = require('../data/db-config');
+
+function get() {
+    return db('tasks');
+}
+
+async function getById(id) {
+    const result = await db('tasks')
+        .where('task_id', id);
+    if(result.length < 1) {
+        return null;
+    }
+    return result;
+}
+
+function add(resource) {
+    return db('tasks')
+        .insert(resource)
+        .then(([id]) => {
+            return getById(id);
+        })
+}
+
+module.exports = {
+    get,
+    add
+};
